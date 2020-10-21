30634634dfa9126f3a0f16faff70268e3c17909b
elinfi <elinfins@outlook.com>
2020-10-21 08:17:00 +0200

    Added call to min_err_rate

--- Prosjektoppgave1/prosjekt1.py
+++ Prosjektoppgave1/prosjekt1.py
@@ -2,7 +2,8 @@ import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from nearest_neighbor import nearest_neighbor2
+from nearest_neighbor import nearest_neighbor
+from min_err_rate import min_err_rate
 
 def train_test_split(data):
     # create boolean index array with True for every even index and False for
@@ -18,7 +19,7 @@ def train_test_split(data):
 
 def readfile(filename):
     # data = pd.read_csv(filename, header=None, sep='  ', index_col=0)
-    data = pd.read_csv(filename, header=None, sep='  ', engine='python')
+    data = pd.read_csv(filename, header=None, sep='\s+', engine='python')
     pd.set_option("display.max_rows", None, "display.max_columns", None)
     # print(data)
     # print(data.to_numpy())
@@ -28,27 +29,13 @@ def readfile(filename):
 
 
 
-data = readfile('ds-1.txt')
+data = readfile('ds-3.txt')
 train, test = train_test_split(data.to_numpy())
 
-nearest_neighbor2(test, train)
+err_rate_d1, err_rate_d2, err_rate_d3, err_rate_d4 = nearest_neighbor(test, train)
+print(err_rate_d1)
+print(err_rate_d2)
+print(err_rate_d3)
+print(err_rate_d4)
 
-
-train_object = train[:, 1:]
-test_object = test[:, 1:]
-
-
-# train_objects = train[:, 1:]
-# NN_class = np.empty(test.shape[0])
-# for i in range(test.shape[0]):
-#     test_object = test[i, 1:]
-#     distance = np.linalg.norm(train_objects - test_object, axis=1, keepdims=True)
-#     index = np.argmin(distance)
-#     NN_class[i] = int(train[index, 0])
-#
-# test_class = test[:, 0]
-#
-# n_wrong = np.sum(test_class - NN_class != 0)
-# n_total = len(test)
-# error_rate = n_wrong/n_total
-# print(error_rate)
+min_err_rate(test, train, [1])
